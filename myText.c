820082ae357c5f2de76f4f2e63670d4561b76340
Roey Honig <roeyhonig94@gmail.com>
2021-04-26 02:20:01 +0300

    dynamic buffer

--- myText.c
+++ myText.c
@@ -3,8 +3,7 @@
 int main()
 {
     int userChoice;
-    char *roeyBuffer = (char *)calloc(bufferbyteSize, sizeof(char));;
-
+    char *roeyBuffer = (char*)calloc(bufferbyteSize, sizeof(char));
     present(userPromptForDataStructure);
     scanf("%d",&userChoice);
     if (userChoice == buffer) {
@@ -12,18 +11,29 @@ int main()
         present(userPpromptForText);
         
         char ch;
-        int i = 0;
-        do 
-        {
-            ch = getchar();
-            if (ch != '\n') {
-                *(roeyBuffer + i) = ch;
-                i++;
-            }
-        } while(ch != EOF); 
-        printf("String = %s", roeyBuffer);
+        int j = 1;
+        while (1) {
+            int i = 0;
+            do 
+            {
+                ch = getchar();
+                if (ch == EOF) 
+                    goto jump;
+                if (ch != '\n') {
+                    *(roeyBuffer + i + (j-1)*bufferbyteSize) = ch;
+                    i++;
+                }
+            } while(i < bufferbyteSize); 
+            j++;
+            roeyBuffer = (char *)realloc(roeyBuffer, bufferbyteSize * j);
+            if (roeyBuffer == NULL)
+                goto jump;
+        }
+    jump: 
+        printf("\nString = %s", roeyBuffer);
+        free(roeyBuffer);
     } else if (userChoice == linkedList) {
-
+        
     } else {
         printf("Non Valid Choice");
     }
