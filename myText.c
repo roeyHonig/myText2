ba4dafd00e2dd11474fd5674fe2ee6486cffd000
Roey Honig <roeyhonig94@gmail.com>
2021-04-26 00:36:00 +0300

    getting 1 charter at a time from an infinite user entered text from stdin and putting that char into an array

--- myText.c
+++ myText.c
@@ -3,9 +3,36 @@
 int main()
 {
     int userChoice;
+    char roeyCharAray[60];
 
     present(userPromptForDataStructure);
     scanf("%d",&userChoice);
+    if (userChoice == buffer) {
+        // init the appropriate data structure
+        present(userPpromptForText);
+        
+        char ch;
+        int i = 0;
+        do 
+        {
+            ch = getchar();
+            if (ch != '\n') {
+                roeyCharAray[i] = ch;
+                i++;
+            }
+        } while(ch != EOF); 
+
+        for (i = 0; i < 60; ++i)
+        {
+            printf("%c", roeyCharAray[i]);
+        } 
+        
+    } else if (userChoice == linkedList) {
+
+    } else {
+        printf("Non Valid Choice");
+    }
+    //printf("%lu\n", sizeof(int));
     printf("\n");
     return 0;
 }
--- myText.h
+++ myText.h
@@ -2,5 +2,8 @@
 
 #define userPromptForDataStructure 1
 #define userPpromptForText 2
+#define buffer 1
+#define linkedList 2
+#define bufferbyteSize 60
 
 void present(int i);
